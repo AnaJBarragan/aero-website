ed78b1f72c8c512604e5e1cbdb7251ea96587aaa
Ana B <ana.barraganm55@gmail.com>
2026-07-22 19:47:29 -0400

    fixed config for proper url routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://anajbarragan.github.io",
+  base: "/aero-website",
   integrations: [react()],
 
   vite: {
